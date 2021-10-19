a6df51a6a3a9badee7e6382a860076780194b5b9
Matti Karppa <mattk@itu.dk>
2021-10-19 10:41:17 +0200

    prune timeouts: stderr instead of stdout for non-processable output

--- prune_timeouts.py
+++ prune_timeouts.py
@@ -2,9 +2,10 @@
 
 import os
 import h5py
+import sys
 
 if __name__ == '__main__':
-    print('Pruning timeouted results...')
+    sys.stderr.write('Pruning timeouted results...\n')
     delete_list = list()
     for root, dirs, files in os.walk('results'):
         for fn in files:
